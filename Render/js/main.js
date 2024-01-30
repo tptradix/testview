20247365e92036e4edc5e39697ba16e58e3ffba6
tptradix <157167631+tptradix@users.noreply.github.com>
2024-01-30 17:27:50 +0300

    Add files via upload

---
+++ Render/js/main.js
@@ -0,0 +1,9 @@
+/* TPTWallet version 1.2 */
+/* Hyper-Side Engine connected*/
+/* created at {14.01.23 14:23}*/
+
+
+function openTether() {
+	var openDep = window.open("", "popup", "width=40, height=40");
+	openDep.document.write("<h1>Hello world</h1>");
+}
